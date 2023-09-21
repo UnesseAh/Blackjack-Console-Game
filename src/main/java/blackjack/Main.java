75b3db3f2afdca735795b64795eae9dd127dff22
UnesseAh <youness.ahasla@gmail.com>
2023-09-21 20:43:08 +0100

    shuffle cards using the three methods 'shuffle_deck_of_cards', 'draw_a_card', 'extract_ith_card'

--- src/main/java/blackjack/Main.java
+++ src/main/java/blackjack/Main.java
@@ -4,17 +4,18 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        Random r = new Random();
         ArrayList<int[]> sortedCards  = createCards(1,1);
-        for (int[] card : sortedCards){
-            System.out.println(Arrays.toString(card));
+
+        recursion(r.nextInt(26),sortedCards);
+        ArrayList<int[]> test = shuffle_deck_of_cards(sortedCards);
+        for (int[] nn : sortedCards){
+            System.out.println(Arrays.toString(nn));
         }
-
-
     }
 
-    public static ArrayList<int[]> createCards(int rank, int suit){
-
+    public static ArrayList<int[]> createCards(int rank, int suit)
+    {
         ArrayList<int[]> cards = new ArrayList<>();
 
         for ( int i = suit ; i < 5; i++ )
@@ -27,4 +28,37 @@ public class Main {
         }
         return cards;
     }
+
+    public static ArrayList<int[]> extract_ith_card(int randNum, ArrayList<int[]> arrayList)
+    {
+        int[] getElementAtIndex = arrayList.get(randNum);
+        arrayList.remove(randNum);
+        arrayList.add(0, getElementAtIndex);
+        return arrayList;
+    };
+
+    public static ArrayList<int[]> draw_a_card(ArrayList<int[]> list)
+    {
+        Random rand = new Random();
+        int randNum = rand.nextInt(51);
+
+        ArrayList<int[]> drawCard = extract_ith_card(randNum, list);
+        return drawCard;
+    };
+
+    public static ArrayList<int[]> shuffle_deck_of_cards(ArrayList<int[]> sortedArray)
+    {
+
+        ArrayList<int[]> suffledCard = draw_a_card(sortedArray);
+        return suffledCard;
+    };
+
+    public static void recursion(int n,ArrayList<int[]> sortedCards){
+        if(n<=0){
+            return ;
+        }
+        recursion(n-1,sortedCards);
+        shuffle_deck_of_cards(sortedCards);
+    }
+
 }
