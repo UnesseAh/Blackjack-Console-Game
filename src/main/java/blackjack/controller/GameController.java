06497ae378b62a86d77439709ecba28240d66828
UnesseAh <youness.ahasla@gmail.com>
2023-10-01 14:26:20 +0100

    extractIthCard function

--- src/main/java/blackjack/controller/GameController.java
+++ src/main/java/blackjack/controller/GameController.java
@@ -8,22 +8,20 @@ import java.util.Random;
 public class GameController {
 
     public static void startGame(int userBet){
-        ArrayList<int[]> sortedCards  = createCards(1,1);
-        ArrayList<int[]> shuffledCard = shuffle_deck_of_cards(sortedCards);
+
+//        ArrayList<int[]> sortedCards  = createCards(1,1);
+//        ArrayList<int[]> shuffledCard = shuffle_deck_of_cards(sortedCards);
 //        for (int[] nn : sortedCards){
 //            System.out.println(Arrays.toString(nn));
 //        }
     }
 
     public static int[][] createDeckOfCards(int startRank, int startSuit){
-
-        int num = startSuit;
+        int NUM_SUITS = 4, NUM_RANKS = 13;
         int size = (13 - startRank + 1) + (4 - startSuit) * 13;
-        int NUM_SUITS = 4;
-        int NUM_RANKS = 13;
         int[][] cards = new int[size][2];
-
         int cardIndex = 0;
+
         for(int suit = startSuit; suit <= NUM_SUITS; suit++){
             for(int rank = startRank; rank <= NUM_RANKS; rank++){
                 cards[cardIndex][0] = rank;
@@ -35,17 +33,49 @@ public class GameController {
         return cards;
     }
 
-    public static ArrayList<int[]> createCards(int rank, int suit){
-        ArrayList<int[]> cards = new ArrayList<>();
+    public static int[][][] extractIthCard(int[][] cards, int index) {
+        int[][] extractedCard = {{cards[index][0], cards[index][1]}};
+        int[][] restOfCards = new int[cards.length - 1][2];
+        int currentIndex = 0;
 
-        for ( int i = suit ; i < 5; i++ ){
-            for ( int j = rank; j < 14; j++ ){
-                cards.add(new int[]{j,i});
-            }
-            rank = 1;
+        int i = 0;
+        while ( i <= cards.length -1 && i != index ){
+            restOfCards[currentIndex] = cards[i];
+            currentIndex++;
+            i++;
         }
-        return cards;
+
+        int[][][] ListOfExtractedAndRestOfCards = { extractedCard, restOfCards };
+
+        return ListOfExtractedAndRestOfCards;
     }
+
+    public static int[][][] drawACard(int[][] cards){
+        Random random = new Random();
+        int randomIndex = random.nextInt(cards.length);
+        return extractIthCard(cards, randomIndex);
+    }
+
+    public static int[][][] shuffleDeckOfCards(int[][] cards)
+    {
+        int[][][] twoArrays = new int[2][52][2];
+        return drawACard(cards);
+
+//        int[][] shuffledCards = new int[52][2];
+//        return shuffledCards;
+    }
+
+//    public static ArrayList<int[]> createCards(int rank, int suit){
+//        ArrayList<int[]> cards = new ArrayList<>();
+//
+//        for ( int i = suit ; i < 5; i++ ){
+//            for ( int j = rank; j < 14; j++ ){
+//                cards.add(new int[]{j,i});
+//            }
+//            rank = 1;
+//        }
+//        return cards;
+//    }
 
     public static ArrayList<int[]> extract_ith_card(int randNum, ArrayList<int[]> arrayList) {
         int[] getElementAtIndex = arrayList.get(randNum);
--- src/main/java/blackjack/view/Main.java
+++ src/main/java/blackjack/view/Main.java
@@ -10,8 +10,8 @@ import static blackjack.view.GameView.lunchGame;
 public class Main {
     public static void main(String[] args){
 //        lunchGame();
-        createDeckOfCards(12,3);
-
+        int[][] sortedCards = createDeckOfCards(1,1);
+        int[][][] shuffledCards = shuffleDeckOfCards(sortedCards);
 //        ArrayList<int[]> test = shuffle_deck_of_cards(sortedCards);
 
     }
